e3e6f39ebf7603ef561641d51056d9bcb7df29cf
mbair87 <91098965+mbair87@users.noreply.github.com>
2022-03-25 17:09:22 -0700

    added forms

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Redirect, useParams } from "react-router-dom";
 
 import ThoughtList from "../components/ThoughtList";
-
-import { useQuery } from "@apollo/client";
+import ThoughtForm from '../components/ThoughtForm';
+import { useQuery, useMutation } from "@apollo/client";
 import { QUERY_USER, QUERY_ME } from "../utils/queries";
 import FriendList from "../components/FriendList";
 import Auth from "../utils/auth";
+import { ADD_FRIEND } from "../utils/mutations";
 
 const Profile = () => {
   const { username: userParam } = useParams();
@@ -16,6 +17,7 @@ const Profile = () => {
   });
 
   const user = data?.me || data?.user || {};
+  const [addFriend] = useMutation(ADD_FRIEND);
 
   // redirect to personal profile page if username is the logged-in user's
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
@@ -27,16 +29,32 @@ const Profile = () => {
   if (!user?.username) {
     return (
       <h4>
-        You need to be logged in to see this page. Use the navigation links above to sign up or log in!
+        You need to be logged in to see this page. Use the navigation links
+        above to sign up or log in!
       </h4>
     );
   }
+
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id },
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
   return (
     <div>
       <div className="flex-row mb-3">
         <h2 className="bg-dark text-secondary p-3 display-inline-block">
           Viewing {userParam ? `${user.username}'s` : "your"} profile.
         </h2>
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
@@ -55,6 +73,7 @@ const Profile = () => {
           />
         </div>
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
